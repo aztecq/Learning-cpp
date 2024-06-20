6a2e0b0152ced907246034cff8ab6e9f0c548af9
aztecq <96198355+aztecq@users.noreply.github.com>
2024-06-20 20:44:00 +0200

    Create rand-lotto-game.cpp

---
+++ Random Number/rand-lotto-game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+using namespace std;
+
+int liczba;
+
+int main() {
+	cout << "Witaj w losowaniu! Za 4 sekundy nastapi zwolnienie blokady." << endl;
+	Sleep(4000);
+	srand(time(NULL));
+
+	for (int i = 1; i <= 6; i++) {
+		liczba = rand() % 49 + 1;
+		Sleep(1000);
+		cout << liczba << "\a" << endl;
+	}
+
+	return 0;
+}
